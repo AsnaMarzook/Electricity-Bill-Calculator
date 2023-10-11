18edd149ea8f96ed20fe28f85ba507c512265b07
Asna Marzook <112109834+AsnaMarzook@users.noreply.github.com>
2023-10-11 19:01:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,25 +5,27 @@ no_units = (streamlit.text_input("Number of Units"))
 outstanding = (streamlit.text_input("Outstanding Value"))
 
 def total_value(value,outstanding):
-  outstanding = int (outstanding)
-  return value+outstanding
+ outstanding = int (outstanding)
+ return value+outstanding
 
 def calculator(no_units):
-  no_units = int(no_units)
-  if no_units>0 and no_units<=30:
-    return no_units*400
-  elif no_units>30 and no_units<=60:
-    return no_units*550
-  elif no_units>60 and no_units<=90:
-    return no_units*650
-  elif no_units>90 and no_units<=120:
-    return no_units*1500
-  elif no_units>120 and no_units<=180:
-    return no_units*1500
-  elif no_units>180:
-    return no_units*200
+ no_units = int(no_units)
+ if no_units<60: 
+ if no_units>0 and no_units<=30:
+ return no_units*30
+ elif no_units>30 and no_units<=60:
+ return no_units*60
+ else:
+ if no_units>60 and no_units<=90:
+ return no_units*90
+ elif no_units>90 and no_units<=120:
+ return no_units*480
+ elif no_units>120 and no_units<=180:
+ return no_units*480
+ elif no_units>180:
+ return no_units*480
 
 if streamlit.button("Calculate"):
-    streamlit.write(total_value(calculator(no_units),int(outstanding)))
+ streamlit.write(total_value(calculator(no_units),int(outstanding)))
 else:
-    pass
+ pass
